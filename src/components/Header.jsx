c03746db5fde1145a371ded795ec4335f0dd2266
BIV2004 <95098509+BIV2004@users.noreply.github.com>
2022-09-12 12:45:47 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <header className='header'>
+        <img src='https://img2.freepng.ru/20180605/efl/kisspng-skyline-building-toronto-toronto-skyline-5b170c9c84b454.2508598415282372125436.jpg' />
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../App.css';
+
+function Profile(){
+    return(
+        <div className='content'>
+        <img src='https://i.imgur.com/zaLxt6Z.jpg'/>
+        <div>
+        ava+deescription
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          Post 1
+        </div>
+        <div>
+          Post 2
+        </div>
+      </div>
+      </div>
+    );
+}
+
+export default Profile;
